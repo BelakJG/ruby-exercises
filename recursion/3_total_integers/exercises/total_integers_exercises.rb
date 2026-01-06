e0411c64f1e1e56094cca14481f37c81964db3cd
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-01-06 15:41:03 -0500

    completed total_integers_exercises

--- recursion/3_total_integers/exercises/total_integers_exercises.rb
+++ recursion/3_total_integers/exercises/total_integers_exercises.rb
@@ -7,4 +7,12 @@ def total_integers(array)
   # NOTE: you may notice that `Array#flatten` would make quick work of this,
   # but you should implement this method without using it. The tests will check
   # to make sure `#flatten` isn't used.
+  return 1 if array.class == Integer
+  return 0 if array.class != Array && array.class != Integer
+  return 0 if array.length == 0
+  sum = 0
+  array.each do |item|
+    sum += total_integers(item)
+  end
+  sum
 end
