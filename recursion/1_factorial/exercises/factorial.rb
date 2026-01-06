2808df7feaf3fb03696847ff284738c3747982d2
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-01-06 12:57:47 -0500

    completed factorial

--- recursion/1_factorial/exercises/factorial.rb
+++ recursion/1_factorial/exercises/factorial.rb
@@ -1,3 +1,10 @@
 def factorial(num)
   # write a recursive method for calculating the [factorial](https://simple.wikipedia.org/wiki/Factorial) of a number
+  if num == 1 || num == 0
+    return 1
+  end
+  if num < 0
+    return nil
+  end
+  num * factorial(num - 1)
 end
